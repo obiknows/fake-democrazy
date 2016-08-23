582220c57902acf0563951e0f09e7ded6dde3fbb
Sam Nnodim <sam.nnodim@gmail.com>
2016-08-23 15:33:40 -0700

    configured sockets & redux to pass data from client to server back to client

--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,6 +1,17 @@
 import Server from 'socket.io';
 
 // create the Redux store
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  // send the whole state tree to every connected person
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  // get the current state on server connection
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS())
+    socket.on('action', store.dispatch.bind(store))
+  });
 }
