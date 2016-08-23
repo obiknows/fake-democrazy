e769aae6c6d93fd77c8f04fa9857f4b69f327780
Sam Nnodim <sam.nnodim@gmail.com>
2016-08-23 11:26:13 -0700

    added getWinners and updated next()

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -5,16 +5,29 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
-// add the next function for state transitions
+// function for getting vote results
+export function getWinners(vote) {
+  if (!vote) return [];
+
+  const [a,b] = vote.get('pair');
+  const aVotes =  vote.getIn(['tally', a], 0);
+  const bVotes =  vote.getIn(['tally', b], 0);
+  if (aVotes > bVotes) return [a];
+  if (aVotes < bVotes) return [b];
+  else                 return [a,b];
+}
+
+// next function for state transitions
 export function next(state) {
-  const entries = state.get('entries');
+  const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')));
   return state.merge({
     vote: Map({pair: entries.take(2)}),
     entries: entries.skip(2)
   });
 }
 
-// add function to allow for voting
+// function to allow for voting
 export function vote(state, entry) {
   // use updateIn() which applies the function to the array
   return state.updateIn(
