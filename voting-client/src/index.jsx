d62734bc26246a18a700f47fcf66c563ea2e1aec
Sam Nnodim <sam.nnodim@gmail.com>
2016-08-24 11:26:20 -0700

    added Results component

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
-// to be susbstituted w/ real data
-const pair = ['Trainspotting', '28 Days Later'];
+const routes = <Route component={App}>
+    <Route path="/results" component={Results} />
+    <Route path="/" component={Voting} />
+</Route>
 
+// create the root Router
 ReactDOM.render(
-  <Voting pair={pair} winner="Trainspotting"/>,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('app')
 )
---
+++ voting-client/test/components/Results_spec.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  renderIntoDocment,
+  scryRenderedDOMComponentsWithTag,
+  Simulate
+} from 'react-addons-test-utils';
+import {List} from 'immutable';
+import Results from '../../src/components/Results';
+import {expect} from 'chai';
+
+describe('Results', () => {
+
+  // test that results are rendered
+  it('renders entries with vote counts or zero', () => {
+    const pair = List.of("Trainspotting","28 Days Later");
+    const tally = Map({"Trainspotting": 5});
+    const component = renderIntoDocment(
+      <Results pair={pair} tally={tally} />
+    );
+
+    const entries = scryRenderedDOMComponentsWithTag(component, 'entry');
+    const [train, days] = entries.map(e => e.textContent);
+
+    expect(entries.length).to.equal(2);
+    expect(train).to.contain('Trainspotting');
+    expect(train).to.contain('5');
+    expect(days).to.contain('28 Days Later');
+    expect(days).to.contain('0');
+
+  })
+
+});
