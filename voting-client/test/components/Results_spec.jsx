58c88bd1bbbf544b14a6c3a2408fa7f226de0235
Sam Nnodim <sam.nnodim@gmail.com>
2016-08-24 16:03:34 -0700

    finished UI

--- voting-client/test/components/Results_spec.jsx
+++ voting-client/test/components/Results_spec.jsx
@@ -5,7 +5,7 @@ import {
   scryRenderedDOMComponentsWithTag,
   Simulate
 } from 'react-addons-test-utils';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 import Results from '../../src/components/Results';
 import {expect} from 'chai';
 
@@ -27,7 +27,33 @@ describe('Results', () => {
     expect(train).to.contain('5');
     expect(days).to.contain('28 Days Later');
     expect(days).to.contain('0');
-
   })
 
+  // test that click on the 'Next' button works
+  it('invokes the next callaback when next button is clicked', () => {
+    let nextInvoked = false;
+    const next = () => nextInvoked = true;
+
+    const pair = List.of('Trainspotting', '28 Days Later');
+    const component = renderIntoDocment(
+      <Results pair={pair}
+               tally={Map()}
+               next={next} />
+    )
+    Simulate.click(ReactDOM.findDOMNode(component.refs.next));
+
+    expect(nextInvoked).to.equal(true);
+  })
+
+  // test that a winner is rendered
+  it('renders the winner when there is one', () => {
+    const component = renderIntoDocment(
+      <Results winner="Trainspotting"
+               pair={["Trainspotting", "28 Days Later"]}
+               tally={Map()} />
+    )
+    const winner = ReactDOM.findDOMNode(component.refs.winner)
+    expect(winner).to.be.ok
+    expect(winner.textContent).to.contain('Trainspotting')
+  })
 });
