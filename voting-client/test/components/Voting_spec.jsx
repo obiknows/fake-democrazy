4b7a8b3437be2dd10d56efb3ebd4158a4afd2754
Sam Nnodim <sam.nnodim@gmail.com>
2016-08-24 07:28:35 -0700

    front end click and label functionality

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {
   renderIntoDocment,
-  scryRenderedDOMComponentsWithTag
+  scryRenderedDOMComponentsWithTag,
+  Simulate
 } from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
 
 describe('Voting', () => {
 
+  // test that buttons are rendered
   it('renders a pair of buttons', () => {
     const component = renderIntoDocment(
       <Voting pair={["Trainspotting","28 Days Later"]} />
@@ -17,5 +19,20 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal('Trainspotting');
     expect(buttons[1].textContent).to.equal('28 Days Later');
+  })
+
+  // test to see that button click works
+  it('invokes callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocment(
+      <Voting pair={['Trainspotting','28 Days Later']}
+        vote={vote}/>
+    )
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Trainspotting');
   })
 });
