d27f86a3218803dd3c9e95d5335ac1125d98e959
Sam Nnodim <sam.nnodim@gmail.com>
2016-08-24 21:59:44 -0700

    we got data flowing in its lit

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -3,24 +3,31 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
-// store
+// create the store
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine','28 Days Laters'],
-      tally: {Sunshine: 2}
-    }
-  }
-})
+// store.dispatch({
+//   type: 'SET_STATE',
+//   state: {
+//     vote: {
+//       pair: ['Sunshine','28 Days Later'],
+//       tally: {Sunshine: 2}
+//     }
+//   }
+// })
 
-// routes
+// create a server connection & get the initial state w/ SET_STATE
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state})
+);
+
+// some routes for that ass
 const routes = <Route component={App}>
     <Route path="/results" component={ResultsContainer} />
     <Route path="/" component={VotingContainer} />
